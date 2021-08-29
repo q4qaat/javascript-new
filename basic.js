22508b83e660987ba48f29322b0cdb8013309cde
shaukat1206 <q4qaat@gmail.com>
2021-08-29 09:17:02 +0530

    new hai basic wala

---
+++ basic.js
@@ -0,0 +1,48 @@
+// print
+// let name = 20;
+// console.log(name);
+// console.log("saarawe");
+
+// variable
+// let a = 10;
+// let b = "bharat";
+// let c = new Date();
+// console.log(c);
+
+let n1 = "molnapur, mohammadpur, siwan , bihar india - 841226";
+const age = 12;
+console.log(n1);
+console.log(n1);
+console.log(n1);
+console.log(n1);
+console.log(n1);
+
+// math sign - operator
+// + , -
+
+// let a = (2 + 4 + 12 - 1) / 2 + 2 * (5 % 3);
+// let b = "rajan" + "kumar";
+
+// console.log(a);
+// console.log(b);
+
+// let a = "shayam";
+// let b = 40;
+
+// console.log(a + b);
+
+// let a = 20 + " raju" + 53 + "7";
+// console.log(a)
+
+// let a = 20 + 70 + "raja" + 40 + 30;
+// console.log(a);
+
+// data types
+// Number , String , Date , Boolean , Array
+// typeof
+
+// let name = {
+//   a: 10,
+//   b: 20
+// };
+// console.log(typeof name.a);
